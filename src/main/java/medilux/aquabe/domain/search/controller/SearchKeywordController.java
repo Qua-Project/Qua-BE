0ecc7bfe25332c805dbb58bc57e6fb431625b2f3
pstar987 <pstar987@naver.com>
2025-03-11 16:05:52 +0900

    feat : 최근 검색어 기능 추가

--- src/main/java/medilux/aquabe/domain/search/controller/SearchKeywordController.java
+++ src/main/java/medilux/aquabe/domain/search/controller/SearchKeywordController.java
@@ -2,8 +2,11 @@ package medilux.aquabe.domain.search.controller;
 
 import lombok.RequiredArgsConstructor;
 import medilux.aquabe.domain.search.dto.PopularKeywordResponse;
+import medilux.aquabe.domain.search.dto.RecentKeywordResponse;
 import medilux.aquabe.domain.search.service.SearchLogService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,5 +21,12 @@ public class SearchKeywordController {
     @GetMapping("/popular")
     public ResponseEntity<List<PopularKeywordResponse>> getPopularKeywords() {
         return ResponseEntity.ok(searchLogService.getTop10SearchKeywords());
+    }
+
+    @GetMapping("/recent")
+    public ResponseEntity<RecentKeywordResponse> getRecentKeywords(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loginEmail = authentication.getName();
+        return ResponseEntity.ok(searchLogService.getRecentSearchKeywords(loginEmail));
     }
 }
